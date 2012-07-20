4ffb81d5c860dfc48742503f30b056d74d7f97d8
Andrew Peterson <andy@ndpsoftware.com>
2012-07-19 22:03:51 -0700

    Upgrade Csster

--- git-cheatsheet/csster.js
+++ git-cheatsheet/csster.js
@@ -1,10 +1,11 @@
 // csster.js
+// version 1.0.2
 // Copyright (c) Andrew J. Peterson / ndpsoftware.com
 // All Rights Reserved
 // 
 // See http://github.com/ndp/csster
 // 
-// Generated Sun Nov 14 15:28:45 PST 2010
+// Generated Fri May 25 20:28:34 PDT 2012
 // 
 // 
 if (!Csster) {
@@ -683,7 +684,7 @@ function clearfix() {
         }
     };
     if (Csster.browser.msie) {
-     css['zoom'] = '1'
+        css['zoom'] = '1'
     }
     return css;
 }
@@ -708,6 +709,79 @@ function verticalCentering(height) {
         marginTop: -(height / 2)
     }
 }
+
+function linearGradient(startingPoint, color1, color2, etc) {
+    var prefix = '',
+            result = '';
+    if (Csster.browser.webkit) {
+        prefix = '-webkit';
+    } else if (Csster.browser.mozilla) {
+        prefix = '-moz';
+    }
+
+
+    var stops = [];
+    for (var i = 0; i < arguments.length; i++) {
+        var argument = arguments[i];
+        if (typeof argument == 'string') {
+            stops.push(argument);
+        } else if ($.isArray(argument)) {
+            for (var j = 0; j < argument.length; j++) {
+                stops.push(argument[j]);
+            }
+        } else {
+            for (p in arguments[i]) {
+                stops.push(argument[p] + (p != 0 && p != '100' ? (' ' + p + '%') : '') );
+            }
+        }
+    }
+
+
+    result = prefix + '-linear-gradient(';
+    for (i = 0; i < stops.length; i++) {
+        if (i !== 0) result += ', ';
+        result += stops[i];
+    }
+    result += ')';
+    return result;
+}
+
+//    },generateLinearGradient:function() {
+//        var props = c.gradientProps,
+//                g = props.type + "-gradient(",e = "";
+//        $sample = c.sample,
+//                gCount = a.getPaletteLength(),
+//                palette = a.getPalette();
+//        if (props.xStart !== props.xEnd) {
+//            g = g + props.xStart + " "
+//        }
+//        if (props.yStart !== props.yEnd) {
+//            g = g + props.yStart
+//        }
+//        g = g + ", ";
+//        var h = c.getColor;
+//        $.each(palette, function(i, j) {
+//            if (i > 0) {
+//                e = e + " "
+//            }
+//            e = e + h(j) + " " + j.position + "%,"
+//        });
+//        g = g + e;
+//        g = g.substr(0, g.length - 1) + ")";
+//        return g
+//    generateWebkitGradient:function() {
+//        var j = c.gradientProps,l = "-webkit-gradient(" + j.type + "," + c.fetchGradientStart() + "," + c.fetchGradientEnd() + ",",g = "";
+//        var e = a.getPalette(),f = e.length,k,m;
+//        for (var h = 0; h < f; h++) {
+//            m = e[h];
+//            k = (m.position / 100);
+//            g = g + "color-stop(" + k + ", rgb(" + m.rgb.r + "," + m.rgb.g + "," + m.rgb.b + ")),"
+//        }
+//        l = l + g;
+//        l = l.substr(0, l.length - 1) + ");";
+//        return l
+
+
 (function() {
 
 
@@ -939,3 +1013,13 @@ Csster.compressSelectors = function(rules) {
 };
 
 Csster.propertyPreprocessors.push(Csster.macroPreprocessor('has'));
+if (typeof jQuery != 'undefined') {
+    (function($) {
+        $.fn.csster = function(rules) {
+            var newRules = {};
+            newRules[this.selector] = rules;
+            Csster.style(newRules);
+            return this;
+        }
+    })(jQuery);
+}
