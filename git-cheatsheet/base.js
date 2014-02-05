95cd44cf95d0eb0deea6670ad176eba9b90fa431
Andrew Peterson <andy@ndpsoftware.com>
2014-02-04 19:49:10 -0800

    simplify utils

--- git-cheatsheet/base.js
+++ git-cheatsheet/base.js
@@ -1,22 +1,22 @@
+// Return value after `val` in the array `a`, looping around
 function next(a, val) {
   var index = a.indexOf(val);
-  if (index == -1 || index == (a.length - 1)) {
-    return a[0];
-  } else {
-    return a[index + 1]
-  }
+  return index == -1 || index == (a.length - 1) ? a[0] : a[index + 1];
 }
 
+// Return value before `val` in the array `a`, looping around
 function prev(a, val) {
   var index = a.indexOf(val);
-  if (index <= 0) {
-    return a[a.length - 1];
-  } else {
-    return a[index - 1]
-  }
+  return index <= 0 ? a[a.length - 1] : a[index - 1];
 }
 
-
+/*
+Escape the string in an app-specific way:
+  <x> become <em>x</em>
+  [x] becomes <span class="optional">x</span>
+  `x` becomes <code>x</code>
+  \r becomes <br>
+*/
 function esc(s) {
   return s.replace(/</g, 'zyx').replace(/>/g, '</em>').replace(/zyx/g, '<em>').
       replace('[', '<span class="optional">').replace(']', '</span>').
@@ -24,6 +24,7 @@ function esc(s) {
       replace(/\r/g, '<br>');
 }
 
+// Return a two charact language code
 function detectLanguage(/*pass in navi*/gator) {
   return (cookies.read('language') ||
       gator.language ||
